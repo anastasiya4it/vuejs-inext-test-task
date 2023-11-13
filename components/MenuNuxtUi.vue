272800e765437848cc1dc7c4625569343ef95a71
Bo136 <anastasiya4it@gmail.com>
2023-11-13 05:41:55 +0200

    Added heroicons to the dropdown menu items

--- components/MenuNuxtUi.vue
+++ components/MenuNuxtUi.vue
@@ -5,14 +5,15 @@
         :items="items"
         :ui="{
           item: {
-            base: 'group flex items-center gap-2 w-full',
-            rounded: 'rounded-md',
             padding: 'px-2 py-2',
-            size: 'text-sm',
             active:
               'bg-violet-500 dark:bg-violet-950 text-white dark:text-gray-200 ', //
             disabled: 'cursor-not-allowed opacity-50',
-            shortcuts: 'hidden md:inline-flex flex-shrink-0 gap-0.5 ms-auto',
+            icon: {
+              base: 'flex-shrink-0 h-4 w-4 ',
+              active: 'text-white dark:text-gray-200',
+              inactive: 'text-gray-400 dark:bg-gray-400',
+            },
           },
           width: 'w-56 max-[230px]:w-[100vw]',
           ring: 'ring-1 ring-gray-100 dark:ring-gray-700',
@@ -36,22 +37,27 @@ const items = [
   [
     {
       label: "Account",
+      icon: "i-heroicons-user-20-solid",
     },
     {
       label: "Edit",
+      icon: "i-heroicons-pencil-square-20-solid",
     },
   ],
   [
     {
       label: "Make a copy",
+      icon: "i-heroicons-document-duplicate-20-solid",
     },
     {
       label: "Archive",
+      icon: "i-heroicons-archive-box-20-solid",
     },
   ],
   [
     {
       label: "Delete",
+      icon: "i-heroicons-trash-20-solid",
     },
   ],
 ];
