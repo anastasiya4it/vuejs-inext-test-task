539ce68da8310b9a2458e576a5bef0a98ba56d12
Bo136 <anastasiya4it@gmail.com>
2023-11-13 05:15:06 +0200

    Replaced the current Headless UI with NuxtUI for the drop down menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,56 @@
 <template>
   <p>Fix me</p>
   <div class="fixed top-16 w-56 max-[230px]:w-full text-right">
-    <Menu as="div" class="relative inline-block text-left">
-      <div>
-        <MenuButton
-          class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-        >
-          Options
-        </MenuButton>
-      </div>
-
-      <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <MenuItems
-          class="absolute right-0 mt-2 w-56 max-[230px]:w-[100vw] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-        >
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Account
-              </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Edit
-              </button>
-            </MenuItem>
-          </div>
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Make a copy
-              </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Archive
-              </button>
-            </MenuItem>
-          </div>
-
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-              >
-                Delete
-              </button>
-            </MenuItem>
-          </div>
-        </MenuItems>
-      </transition>
-    </Menu>
+    <UDropdown
+      :items="items"
+      :ui="{
+        item: {
+          base: 'group flex items-center gap-2 w-full',
+          rounded: 'rounded-md',
+          padding: 'px-2 py-2',
+          size: 'text-sm',
+          active:
+            'bg-violet-500 dark:bg-violet-950 text-white dark:text-gray-200 ', //
+          disabled: 'cursor-not-allowed opacity-50',
+          shortcuts: 'hidden md:inline-flex flex-shrink-0 gap-0.5 ms-auto',
+        },
+        width: 'w-56 max-[230px]:w-[100vw]',
+        ring: 'ring-1 ring-gray-100 dark:ring-gray-700',
+      }"
+      :popper="{ placement: 'bottom-end' }"
+    >
+      <UButton
+        :class="[
+          'inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75',
+          'dark:bg-violet-400  hover:dark:bg-violet-500 focus-visible:ring-white/75 text-white',
+        ]"
+        label="Options"
+      />
+    </UDropdown>
   </div>
 </template>
-
 <script setup>
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+const items = [
+  [
+    {
+      label: "Account",
+    },
+    {
+      label: "Edit",
+    },
+  ],
+  [
+    {
+      label: "Make a copy",
+    },
+    {
+      label: "Archive",
+    },
+  ],
+  [
+    {
+      label: "Delete",
+    },
+  ],
+];
 </script>
